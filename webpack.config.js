f65bdd48303098009e3056b4259d6dcd811d925b
Christophe Rosset <tophe@topheman.com>
2015-03-23 23:26:24 +0100

    webpack config ok for jsx and sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+'use strict';
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-  entry: "./src/components/App.jsx",
+  entry: {
+    bundle: "./src/components/App.jsx",
+    main: "./src/style/main.scss"
+  },
   output: {
-    path: './build/js',
-    publicPath: "/js/",
-    filename: "bundle.js"
+    path: './build/assets',
+    publicPath: "/assets/",
+    filename: "js/[name].js"
   },
   devtool: "#inline-source-map",
   devServer: {
@@ -11,7 +18,15 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'}
+      {test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {
+        test: /\.scss/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      }
     ]
-  }
+  },
+  plugins: [
+    // extract inline css into separate 'styles.css'
+    new ExtractTextPlugin('css/[name].css')
+  ]
 };
