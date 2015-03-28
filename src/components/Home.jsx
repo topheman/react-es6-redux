4ef8be2a5a12f3bddd184ceba8b457992c5e4fb1
Christophe Rosset <tophe@topheman.com>
2015-03-28 01:17:19 +0100

    some content on the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,9 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        This is the Home page
+        <p>Welcome to my project study on React + ES6. This is a work in progress.</p>
+        <p>I also use webpack + babel for the module loader / transpiling part.</p>
+        <p>If you want to know more, you can find out the next steps on the <a href="https://github.com/topheman/react-es6" title="react-es6 on github">github repo</a></p>
       </div>
     );
   }
