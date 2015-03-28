a2b03710ff03cbc1b3352c59c77c8d687b0cc292
Christophe Rosset <tophe@topheman.com>
2015-03-28 02:28:49 +0100

    typo

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Router from 'react-router';
-import routes from 'routes.jsx';
+import routes from './routes.jsx';
 
 Router.run(routes, function(Handler){
   React.render(<Handler/>, document.body);
