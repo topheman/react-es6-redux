b6bd6f34f4492e8ac726fde3323bc2f32d984288
Christophe Rosset <tophe@topheman.com>
2015-03-29 18:34:22 +0200

    added some infos in home and intro

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import Router from 'react-router';
+
+var Link = Router.Link;
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome to my project study on React + ES6. This is a work in progress.</p>
-        <p>I also use webpack + babel for the module loader / transpiling part.</p>
-        <p>If you want to know more, you can find out the next steps on the <a href="https://github.com/topheman/react-es6" title="react-es6 on github">github repo</a></p>
+        <p>This project is a POC with the following constraints :</p>
+        <ul>
+          <li>Use React</li>
+          <li>Use ES6</li>
+        </ul>
+        <p>This is the second step to a bigger project : <strong>making an isomorphic app</strong> with those technologies
+          (the first step being my previous project : <a href="https://github.com/topheman/topheman-apis-proxy" title="topheman-apis-proxy on github">topheman-apis-proxy</a> which I'm relying on to access data like the one provided
+          by the Github API).
+        </p>
+        <p>This is a work in progress, if you want to know more, you can find out the next steps on the <a href="https://github.com/topheman/react-es6" title="react-es6 on github">github repo</a>.</p>
+        <p><strong>TLDR;</strong> : click on the button to try it ! (still WIP)</p>
+        <p className="text-center"><Link className="btn btn-default btn-primary btn-lg" to="github">TRY the DAMN thing !</Link></p>
       </div>
     );
   }
--- src/components/github/IntroBox.jsx
+++ src/components/github/IntroBox.jsx
@@ -6,7 +6,9 @@ export default class IntroBox extends React.Component {
   render(){
     return(
       <div>
-        <p>This is where there could be some introduction / explanation about this feature ...</p>
+        <p>This is only a simple feature, search Github users ...</p>
+        <p>All the UI is coded in React and ES6, using only isomorphic techs like superagent for the AJAX request (so that it could also work server-side).</p>
+        <p>It's not finished, you'll be able to browse user profile on an other route (will add some UI features such as spinners).</p>
       </div>
     );
   }
