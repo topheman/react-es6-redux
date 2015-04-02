11122291385fe5047f4c658b6f845f25af163304
Christophe Rosset <tophe@topheman.com>
2015-04-02 23:35:13 +0200

    fixed an error handling in services/http.js

--- src/services/http.js
+++ src/services/http.js
@@ -9,23 +9,23 @@ export default {
     var promise = new Promise(function(resolve, reject){
       request.get(configuration.backendBaseUrl+relativeUrl).set('Accept', 'application/json')
         .end(function(err, res){
-          if(err){
-            if(res && res.headers && typeof res.headers['x-ratelimit-remaining'] !== 'undefined'){
-              reject({
+          if(err || !res){
+            if(typeof res !== 'undefined' && res.headers && typeof res.headers['x-ratelimit-remaining'] !== 'undefined'){
+              return reject({
                 type: 'rateLimit',
                 message: err,
                 humanMessage: "The server is very crowded, please try again in a few seconds."
               })
             }
             else {
-              reject({
+              return reject({
                 type: "error",
-                message: err,
+                message: err ? err : 'No response',
                 humanMessage: "An error occured, please try again."
               });
             }
           }
-          resolve({
+          return resolve({
             data: res.body,
             infos: {
               ratelimitRemaining: res.headers['x-ratelimit-remaining']
