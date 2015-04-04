9445919716a0e305da9e1e7ee1452da802fd39ef
Christophe Rosset <tophe@topheman.com>
2015-04-04 10:03:51 +0200

    refactor correct name SeachBox in Github.jsx

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import IntroBox from './github/IntroBox.jsx';
-import SearchUsersBox from './github/SearchBox.jsx';
+import SearchBox from './github/SearchBox.jsx';
 
 export default class Github extends React.Component {
   render() {
     return (
       <div>
         <IntroBox/>
-        <SearchUsersBox/>
+        <SearchBox/>
       </div>
     )
   }
