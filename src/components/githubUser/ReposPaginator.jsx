f2c1747bc91df374d8372d3d2792d5c35ab146c0
Christophe Rosset <tophe@topheman.com>
2015-04-09 23:00:31 +0200

    reformat ReposPaginator.jsx

--- src/components/githubUser/ReposPaginator.jsx
+++ src/components/githubUser/ReposPaginator.jsx
@@ -3,72 +3,79 @@
 import React from 'react';
 
 export default class ReposPaginator extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {};
-		this.gotoPage = this.gotoPage.bind(this);
-		this.gotoNextPage = this.gotoNextPage.bind(this);
-		this.gotoPreviousPage = this.gotoPreviousPage.bind(this);
-		this.gotoFirstPage = this.gotoFirstPage.bind(this);
-		this.gotoLastPage = this.gotoLastPage.bind(this);
-		this.handleClickGotoPage = this.handleClickGotoPage.bind(this);
-	}
-	gotoPage(pageNum){
-		console.log('ReposPaginator.gotoPage',pageNum);
-	}
-	gotoNextPage(){
-		this.gotoPage(this.props.infos.page+1);
-	}
-	gotoPreviousPage(){
-		this.gotoPage(this.props.infos.page-1);
-	}
-	gotoFirstPage(){
-		this.gotoPage(1);
-	}
-	gotoLastPage(){
-		this.gotoPage(this.props.infos.totalPages);
-	}
-	handleClickGotoPage(e){
-		e.preventDefault();
-		console.log(e.target.className);
-		var accepted = ['gotoFirstPage',
-		'gotoPreviousPage',
-		'gotoNextPage',
-		'gotoLastPage'];
-		if(accepted.indexOf(e.target.className) > -1){
-			this[e.target.className]();
-		}
-	}
-	render(){
-		var infos = this.props.infos;
-		console.log('infos',infos);
-		if(infos.totalPages > 1){
-			var pages, currentPage;
-			var firstPage,
-					previousPage,
-					nextPage,
-					lastPage;
-			if(infos.page < infos.totalPages){
-				nextPage = (
-					<li className="next">
-						<a href="#" title="first page" onClick={this.handleClickGotoPage} className="gotoNextPage">
-							<span aria-hidden="true">&rarr;</span>
-						</a>
-					</li>
-				);
-			}
-			return (
-				<nav>
-					<ul className="pager">
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.gotoPage = this.gotoPage.bind(this);
+    this.gotoNextPage = this.gotoNextPage.bind(this);
+    this.gotoPreviousPage = this.gotoPreviousPage.bind(this);
+    this.gotoFirstPage = this.gotoFirstPage.bind(this);
+    this.gotoLastPage = this.gotoLastPage.bind(this);
+    this.handleClickGotoPage = this.handleClickGotoPage.bind(this);
+  }
+
+  gotoPage(pageNum) {
+    console.log('ReposPaginator.gotoPage', pageNum);
+  }
+
+  gotoNextPage() {
+    this.gotoPage(this.props.infos.page + 1);
+  }
+
+  gotoPreviousPage() {
+    this.gotoPage(this.props.infos.page - 1);
+  }
+
+  gotoFirstPage() {
+    this.gotoPage(1);
+  }
+
+  gotoLastPage() {
+    this.gotoPage(this.props.infos.totalPages);
+  }
+
+  handleClickGotoPage(e) {
+    e.preventDefault();
+    console.log(e.target.className);
+    var accepted = ['gotoFirstPage',
+      'gotoPreviousPage',
+      'gotoNextPage',
+      'gotoLastPage'];
+    if (accepted.indexOf(e.target.className) > -1) {
+      this[e.target.className]();
+    }
+  }
+
+  render() {
+    var infos = this.props.infos;
+    console.log('infos', infos);
+    if (infos.totalPages > 1) {
+      var pages, currentPage;
+      var firstPage,
+        previousPage,
+        nextPage,
+        lastPage;
+      if (infos.page < infos.totalPages) {
+        nextPage = (
+          <li className="next">
+            <a href="#" title="first page" onClick={this.handleClickGotoPage} className="gotoNextPage">
+              <span aria-hidden="true">&rarr;</span>
+            </a>
+          </li>
+        );
+      }
+      return (
+        <nav>
+          <ul className="pager">
 					  {nextPage}
-					</ul>
-				</nav>
-			);
-		}
-		else{
-			return (
-				<p></p>
-			);
-		}
-	}
+          </ul>
+        </nav>
+      );
+    }
+    else {
+      return (
+        <p></p>
+      );
+    }
+  }
 }
