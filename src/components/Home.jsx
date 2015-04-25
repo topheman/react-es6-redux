8649e62a748513ef919e76b46b2c2ecc623c034b
Christophe Rosset <tophe@topheman.com>
2015-04-26 01:10:06 +0200

    updated Home.jsx content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,5 @@
+'use strict';
+
 import React from 'react';
 import Router from 'react-router';
 
@@ -16,9 +18,13 @@ export default class Home extends React.Component {
           (the first step being my previous project : <a href="https://github.com/topheman/topheman-apis-proxy" title="topheman-apis-proxy on github">topheman-apis-proxy</a> which I'm relying on to access data like the one provided
           by the Github API).
         </p>
-        <p>This is a work in progress, if you want to know more, you can find out the next steps on the <a href="https://github.com/topheman/react-es6" title="react-es6 on github">github repo</a>.</p>
-        <p><strong>TLDR;</strong> : click on the button to try it ! (still WIP)</p>
+        <p>
+          The project is pretty much finished, <a href="https://github.com/topheman/react-es6/issues/2" title="Github issue on Flux implementation for this project">flux could be implemented for state management</a>.
+          You can access the source code on the <a href="https://github.com/topheman/react-es6" title="react-es6 on github">github repo</a> and also read <a href="http://dev.topheman.com/playing-with-es6-and-react/" title="Playing with React and ES6">the blog post</a> about it.
+        </p>
+        <p><strong>TL;DR</strong> : click on the button to try it !</p>
         <p className="text-center"><Link className="btn btn-default btn-primary btn-lg" to="github">TRY the DAMN thing !</Link></p>
+        <p>The third step (turning the app to isomorphic is now finished, you can try <a href="https://topheman-react-es6-isomorphic.herokuapp.com/" title="Try topheman/react-es6-isomorphic">topheman/react-es6-isomorphic - demo</a>)</p>
       </div>
     );
   }
