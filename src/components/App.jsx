9ca096a231bc9fa65b68903958f8af5f471ccce2
Christophe Rosset <tophe@topheman.com>
2015-10-25 02:37:33 +0200

    feat(*) send routerState accross the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,39 @@
 'use strict';
 
-import React from 'react';
-//import Router from 'react-router';
-//var RouteHandler = Router.RouteHandler;
+import React, { PropTypes } from 'react';
+
+import { connect } from 'react-redux';
 
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 
-const App = ({children}) => (
-  <div>
-    <Header title="react-es6"/>
-    <div className="container">
-      {children}
-    </div>
-    <Footer/>
-  </div>
-);
+//@connect(state => ({ routerState: state.router })) - bellow the ES6 version without using decorators
+var App = (function(){
+
+  class App extends React.Component {
+
+    render() {
+      return (
+        <div>
+          <Header title="react-es6"/>
+          <div className="container">
+            {this.props.children}
+          </div>
+          <Footer/>
+        </div>
+      );
+    }
+
+  }
+
+  App.propTypes = {
+    children: PropTypes.node
+  };
+
+  App = connect(state => ({ routerState: state.router }))(App);
+
+  return App;
+
+})();
 
 export default App;
