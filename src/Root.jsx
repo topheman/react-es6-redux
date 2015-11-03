e26bb9806e70ef83de3395f1c7242e209ed15ef6
Christophe Rosset <tophe@topheman.com>
2015-11-04 00:37:45 +0100

    feat(*) remove unused code

--- src/Root.jsx
+++ src/Root.jsx
@@ -6,20 +6,24 @@ import routes from './routes.jsx';
 
 import configureStore from './utils/configure-store.js';
 
-import { createStore, compose, combineReducers } from 'redux';
-import { ReduxRouter, routerStateReducer, reduxReactRouter } from 'redux-router';
-import { Provider, connect } from 'react-redux';
-import { devTools } from 'redux-devtools';
+import { ReduxRouter } from 'redux-router';
+import { Provider } from 'react-redux';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 const initialState = {
   counter: 0
 };
 
+/**
+ * The whole store/reducers/actions configuration is done here
+ * The configuration of the router is also done their since it's stored in redux store (using a specific reducer)
+ */
 export const store = configureStore(initialState);
 
 function getDebugPanel(){
   if(__DEVTOOLS__){
+    console.info('redux devtools active, to hide the panel : ctrl+H');
+    console.info('for more infos', 'https://github.com/gaearon/redux-devtools');
     return (
       <DebugPanel top right bottom>
         <DevTools store={store} monitor={LogMonitor} />
