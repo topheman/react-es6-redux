64046363bb4aa17230aaeecf0101cc67738a9e97
Christophe Rosset <tophe@topheman.com>
2015-11-10 22:00:57 +0100

    feat(*) optimize bundle weight by not requiring devtools in prod
    By using webpack.DefinePlugin that lets you inject variable at transpile time, I can drop the unecessary parts of code (devtools related) which are not meant to be in the production bundle. This is done by:
    * using CommonJS over static ES6 imports on those specific places
    * relying on __DEVTOOLS__ variable I defined in webpack which statement will be dropped at minification if equal to false
    
    See more in the diff
    
    https://webpack.github.io/docs/list-of-plugins.html#defineplugin

--- src/Root.jsx
+++ src/Root.jsx
@@ -8,7 +8,6 @@ import configureStore from './utils/configure-store.js';
 
 import { ReduxRouter } from 'redux-router';
 import { Provider } from 'react-redux';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 const initialState = {
   counter: 0
@@ -20,15 +19,28 @@ const initialState = {
  */
 export const store = configureStore(initialState);
 
+/**
+ * Thanks to webpack.DefinePlugin which lets you inject variables at transpile time,
+ * everything inside the if statement will be dropped at minification if __DEVTOOLS__ is set to false.
+ * This is why I don't use static ES6 import but CommonJS import (so that it will only get required in that particular case)
+ *
+ * Cause: since the following are debug tools, they are not meant to be a part of the production bundle (which makes it lighter)
+ *
+ * https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+ */
 function getDebugPanel(){
   if(__DEVTOOLS__){
-    console.info('redux devtools active, to hide the panel : ctrl+H');
-    console.info('for more infos', 'https://github.com/gaearon/redux-devtools');
+    console.info('redux devtools active, to hide the panel : ctrl+H - for more infos', 'https://github.com/gaearon/redux-devtools');
+    const { DevTools, DebugPanel, LogMonitor } = require('redux-devtools/lib/react');
     return (
       <DebugPanel top right bottom>
         <DevTools store={store} monitor={LogMonitor} />
       </DebugPanel>
     );
+  }
+  else{
+    console.info('redux devtools not active, you can test them online at','https://topheman.github.io/react-es6-redux/devtools.html');
+    console.info('if you\'re testing the project in local, please refer to the README to activate them');
   }
   return(<div/>);
 }
