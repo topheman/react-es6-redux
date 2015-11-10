4e9192c4cb86d1322a07244227a1670cb1f9df50
Christophe Rosset <tophe@topheman.com>
2015-11-10 20:27:11 +0100

    feat(*) combine sub reducers of singleUser

--- src/reducers/singleUser.js
+++ src/reducers/singleUser.js
@@ -1,75 +1,38 @@
 'use strict';
 
-import { merge } from 'lodash';
+import { combineReducers } from 'redux';
 
 import * as constants from '../constants.js';
 
 /**
- * Main reducer
- * (This part could be simplier I flattened the structure - I wouldn't need a main reducer and two subreducers)
- */
-export default function singleUser(state, action) {
-  state = state || {
-      profile:{
-        pristineLogin: '',
-        data: null,
-        fetching: false
-      },
-      repositories: {
-        pristineLogin: '',
-        data: null,
-        fetching: false
-      }
-    };
-  switch (action.type){
-    case constants.INIT_USER_PROFILE:
-      return merge({}, state, {
-        profile:{
-          pristineLogin: action.username
-        },
-        repositories:{
-          pristineLogin: action.username
-        }
-      });
-    case constants.FETCH_USER_PROFILE_INFOS:
-    case constants.FETCH_USER_PROFILE_INFOS_ERROR:
-    case constants.FETCH_USER_PROFILE_INFOS_SUCCESS:
-    case constants.FETCH_USER_PROFILE_REPOSITORIES:
-    case constants.FETCH_USER_PROFILE_REPOSITORIES_ERROR:
-    case constants.FETCH_USER_PROFILE_REPOSITORIES_SUCCESS:
-      return Object.assign({},{
-        profile: profileInfosReducer(state.profile, action),
-        repositories: profileRepositoriesReducer(state.repositories, action)
-      });
-    default:
-      return state;
-  }
-}
-
-/**
- * Sub reducers - since data structure is not flat, to keep immutability AND readability
- * I broke the main reducer into subreducers
- */
-
-/**
  * Receives and returns only a part of the state : state.profile, but doesn't have to know about it
  */
-function profileInfosReducer(state, action) {
+function profile(state, action) {
+  state = state || {
+    pristineLogin: '',
+    data: null,
+    fetching: false,
+    error: null
+  };
   switch(action.type) {
+    case constants.INIT_USER_PROFILE:
+      return Object.assign({},state,{
+        pristineLogin: action.username,
+        data: null,
+        fetching: false,
+        error: null
+      });
     case constants.FETCH_USER_PROFILE_INFOS:
-      return Object.assign({},{
-        pristineLogin: state.pristineLogin,
+      return Object.assign({},state,{
         fetching: true
       });
     case constants.FETCH_USER_PROFILE_INFOS_SUCCESS:
-      return Object.assign({},{
-        pristineLogin: state.pristineLogin,
+      return Object.assign({},state,{
         fetching: false,
         data: action.json.data
       });
     case constants.FETCH_USER_PROFILE_INFOS_ERROR:
-      return Object.assign({},{
-        pristineLogin: state.pristineLogin,
+      return Object.assign({},state,{
         fetching: false,
         error : action.error.humanMessage
       });
@@ -81,23 +44,35 @@ function profileInfosReducer(state, action) {
 /**
  * Receives and returns only a part of the state : state.profile, but doesn't have to know about it
  */
-function profileRepositoriesReducer(state, action) {
+function repositories(state, action) {
+  state = state || {
+      pristineLogin: '',
+      data: null,
+      fetching: false,
+      infos: null,
+      error: null
+    };
   switch(action.type) {
+    case constants.INIT_USER_PROFILE:
+      return Object.assign({},state,{
+        pristineLogin: action.username,
+        data: null,
+        fetching: false,
+        infos: null,
+        error: null
+      });
     case constants.FETCH_USER_PROFILE_REPOSITORIES:
-      return Object.assign({},{
-        pristineLogin: state.pristineLogin,
+      return Object.assign({},state,{
         fetching: true
       });
     case constants.FETCH_USER_PROFILE_REPOSITORIES_SUCCESS:
-      return Object.assign({},{
-        pristineLogin: state.pristineLogin,
+      return Object.assign({},state,{
         fetching: false,
         data: action.json.data,
         infos: action.json.infos
       });
     case constants.FETCH_USER_PROFILE_REPOSITORIES_ERROR:
-      return Object.assign({},{
-        pristineLogin: state.pristineLogin,
+      return Object.assign({},state,{
         fetching: false,
         error : action.error.humanMessage
       });
@@ -105,3 +80,13 @@ function profileRepositoriesReducer(state, action) {
       return state;
   }
 }
+
+/**
+ * Previous reducers are combined together in a higher reducer that is at the level state.singleUser
+ */
+const singleUser = combineReducers({
+  profile,
+  repositories
+});
+
+export default singleUser;
