5fc475bcb5b7f1dd1fa6a27c216e9579f7ee0325
Christophe Rosset <tophe@topheman.com>
2015-11-10 22:26:22 +0100

    dropped some weight ;-)

--- assets/css/main-84bf77bd16da0e06e014.css
+++ assets/css/main-84bf77bd16da0e06e014.css
@@ -3,8 +3,8 @@
  * 
  * A simple app to try React / ES6 & redux, using topheman-apis-proxy as data api backend
  * 
- * @version v2.0.0 - 08/11/2015
- * @revision #51c1093 - https://github.com/topheman/react-es6-redux/tree/51c109341de4f6fc8d54fc03b954aa51c3bbe518
+ * @version v2.0.0 - 10/11/2015
+ * @revision #cef5861 - https://github.com/topheman/react-es6-redux/tree/cef586102a597987806feffb9f6d685b6aaf8b05
  * @author Christophe Rosset
  * @copyright 2015(c) Christophe Rosset
  * @license MIT
