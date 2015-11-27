bb66a4fcafc997b9cd9ffe360745f28865adabb6
Christophe Rosset <tophe@topheman.com>
2015-11-27 20:09:18 +0100

    feat(*) ping the backend in production at app startup
    In production, the backend is on a heroku VM (free plan), which goes asleep after a certain amount of time.
    So the first request from the first user will take about 6 secs (the time for the VM to startup).
    That way, we get a head start.

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -30,6 +30,12 @@ export default {
         throw new Error("First time you instantiate the httpService singleton, you must pass the configuration");
       }
       instance = new HttpService(configuration);
+      // in production, the backend is on a heroku VM,
+      // a simple ping will get it awake before the user really uses the github api
+      // it will avoid 6secs startup (for the first user)
+      if(process.env.NODE_ENV === 'production') {
+        instance.get('/?format=json');
+      }
     }
     return instance;
   }
