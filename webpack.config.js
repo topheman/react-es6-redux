064434f251db265235c9ca3daa64d19158f0c3b2
Christophe Rosset <tophe@topheman.com>
2015-12-05 19:06:47 +0100

    feat(*) generate http fixtures via nock
    npm run generate-http-fixtures will generate a test/fixtures/http.json file with all the mocked requests (containing any infos for the stub - requests headers, response headers / body ...)

--- webpack.config.js
+++ webpack.config.js
@@ -166,7 +166,13 @@ var config = {
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
-  plugins: plugins
+  plugins: plugins,
+  node:{
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
 };
 
 module.exports = config;
