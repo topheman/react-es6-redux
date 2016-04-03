5016ef28aa5bc95157790eb2c6c1c609473b31df
Christophe Rosset <tophe@topheman.com>
2016-04-03 18:02:47 +0200

    test(*): add tests for components/common

---
+++ src/components/common/__tests__/DisplayInfosPanel.spec.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-expressions, react/prop-types */
+import { expect } from 'chai';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import DisplayInfosPanel from '../DisplayInfosPanel';
+import Spinner from '../Spinner';
+import Panel from '../Panel';
+
+describe('components/common/DisplayInfosPanel', () => {
+  describe('state/render', () => {
+    it('should render a spinner when props.infos.fetching=true', () => {
+      const wrapper = shallow(<DisplayInfosPanel infos={{fetching: true}} originalTitle={""} />);
+      expect(wrapper.find(Spinner).props().fetching).to.be.true;
+    });
+    it('should render a panel with a title=props.originalTitle when props.infos.fetching=true', () => {
+      const wrapper = shallow(<DisplayInfosPanel infos={{fetching: true}} originalTitle={"Hello World"} />);
+      expect(wrapper.find(Panel).props().title).to.equal('Hello World');
+    });
+    it('should render the error passed in props.infos.error', () => {
+      const wrapper = shallow(<DisplayInfosPanel infos={{error: 'Something went wrong'}} />);
+      expect(wrapper.contains(<div className="alert alert-danger col-xs-offset-1 col-xs-10" role="alert">Something went wrong</div>)).to.be.true;
+    });
+  });
+});
---
+++ src/components/common/__tests__/DisplayStars.spec.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions, react/prop-types */
+import { expect } from 'chai';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import DisplayStars from '../DisplayStars';
+
+describe('components/common/DisplayStars', () => {
+  describe('state/render', () => {
+    it('should render the number passed via props.number if >0', () => {
+      const wrapper = shallow(<DisplayStars number={2} />);
+      expect(wrapper.find('span').first().text()).to.equal('2 ');
+    });
+    it('should render nothing if props.number=0', () => {
+      const wrapper = shallow(<DisplayStars number={0} />);
+      expect(wrapper.equals(<noscript/>)).to.be.true;
+    });
+  });
+});
